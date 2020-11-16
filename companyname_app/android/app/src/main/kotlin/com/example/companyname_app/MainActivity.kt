49b306c0855e7eacb3e5ec962862371513f61ed7
unknown <papago2355@gmail.com>
2020-11-16 16:44:27 +0900

    first

---
+++ companyname_app/android/app/src/main/kotlin/com/example/companyname_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.companyname_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ first_commit/android/app/src/main/kotlin/com/example/first_commit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_commit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
